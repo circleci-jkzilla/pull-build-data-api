2352a5a95776e7c99f363555295f38d3152ed456
johanna <johanna@circleci.com>
2022-07-18 10:16:01 -0700

    test

--- src/scripts/getaschedule.sh
+++ src/scripts/getaschedule.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo Hello "${PARAM_TO}"
+
+curl 'https://circleci.com/api/v2/project/gh/circleci-demo-javascript-react-app/pipeline?branch=main' -H 'Circle-Token:${CIRCLE_TOKEN}' | jq -r '.pipelines[].id'
